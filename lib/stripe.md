82b39643d414fb5434f84fb88d7188868205b4d9
Jon Phenow <jon@jphenow.com>
2022-10-28 16:41:05 -0500

    Stripe stub

---
+++ lib/stripe.md
@@ -0,0 +1,38 @@
+# Stripe API lib
+
+This is a thin lib meant to look a lot like the Stripe Ruby library.
+This is meant to help you focus on the problem, rather than learning the Stripe API.
+
+## Usage
+
+```ruby
+invoice = Stripe::Invoice.create(customer: "s_asdfasd")
+
+invoice_item = Stripe::InvoiceItem.create(
+  invoice: invoice.id,
+  unit_amount_decimal: 0.1,
+  quantity: 1,
+)
+
+# These stubbing strategies are available for both Invoice and InvoiceItem.
+# We would suggest using these to prove things work in tests or for
+# tinkering in the rails console.
+Stripe::Invoice.retrieve_with({ customer: "s_asdfasd" }) do
+  Stripe::Invoice.retrieve("asdf")
+  # => #<Stripe::Invoice id: asdf, customer: s_asdfasd>
+end
+Stripe::Invoice.retrieve("asdf")
+# => InvalidRequestError (because it couldn't "find" anything)
+
+Stripe::Invoice.slow_with(10)
+  Stripe::Invoice.retrieve("asdf") # waits 10 seconds
+end
+
+Stripe::Invoice.error_with(new StandardError("broken")) do
+  Stripe::Invoice.retrieve("asdf")
+  # => StandardError("broken")
+
+  Stripe::Invoice.retrieve("asdf")
+  # => StandardError("broken")
+end
+```
---
+++ lib/stripe.rb
@@ -0,0 +1,117 @@
+module Stripe
+  InvalidRequestError = Class.new(StandardError)
+
+  module Harness
+    module ClassMethods
+      def error_with(error)
+        @errors ||= []
+        len = @errors.length
+        @errors << error
+        yield
+      ensure
+        @errors.pop if len > @errors.try(:length).to_i
+      end
+
+      def slow_with(time_seconds)
+        @slow_times ||= []
+        len = @slow_times.length
+        @slow_times << time_seconds
+        yield
+      ensure
+        @slow_times.pop if len > @slow_times.try(:length).to_i
+      end
+
+      def retrieve_with(params)
+        @retrieve_params ||= []
+        len = @retrieve_params.length
+        @retrieve_params << params
+        yield
+      ensure
+        @retrieve_params.pop if @retrieve_params.try(:length).to_i > len
+      end
+
+      def create(params)
+        check_error!
+        check_slow!
+
+        new(params)
+      end
+
+      def retrieve(id)
+        check_error!
+        check_slow!
+
+        if @retrieve_params.try(:length).to_i > 0
+          new({id:}.merge(@retrieve_params.last))
+        else
+          raise Stripe::InvalidRequestError, "No such invoice: '#{id}'"
+        end
+      end
+
+      def check_slow!
+        sleep @slow_times.last if @slow_times.try(:length).to_i > 0
+      end
+
+      def check_error!
+        raise @errors.last if @errors.try(:length).to_i > 0
+      end
+    end
+
+    def update_attributes(params)
+      self.class.check_error!
+      self.class.check_slow!
+
+      assign_attributes(params)
+    end
+  end
+
+  class Invoice
+    extend Harness::ClassMethods
+    include Harness
+
+    attr_reader :customer
+
+    def initialize(params)
+      @id = params[:id]
+    end
+
+    def id
+      @id ||= SecureRandom.base36
+    end
+
+    private
+
+    def assign_attributes(params)
+      @customer = params[:customer]
+      @invoice = params[:invoice]
+      @unit_amount_decimal = params[:unit_amount_decimal]
+      @quantity = params[:quantity]
+    end
+  end
+
+  class InvoiceItem
+    extend Harness::ClassMethods
+    include Harness
+
+    attr_reader :invoice
+    attr_reader :unit_amount_decimal
+    attr_reader :quantity
+
+    def initialize(params)
+      @id = params[:id]
+      assign_attributes(params)
+    end
+
+    def id
+      @id ||= SecureRandom.base36
+    end
+
+    private
+
+    def assign_attributes(params)
+      @invoice = params[:invoice]
+      @unit_amount_decimal = params[:unit_amount_decimal]
+      @quantity = params[:quantity]
+    end
+  end
+end
